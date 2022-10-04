2fca0dba1099102b1fba7a5aab343542559fa924
Tyler Morgan <clickjaw@outlook.com>
2022-10-04 10:01:45 -0500

    first push

--- codeAlong1/Program.cs
+++ codeAlong1/Program.cs
@@ -1,4 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace codeAlong1
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            string firstName = "Tyler";
+            int age = 33;
+            string bio = "I'm in code401. We're learning C#.";
+
+            Console.WriteLine($"My name is {firstName}. I'm {age} years old and {bio}.");
+
+
+
+        }
+    }
+
+};
+
 
 
